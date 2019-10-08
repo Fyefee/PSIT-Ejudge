3ae81f5bf705d92df5303befa7eb5f2dcdceb5ef
Felzj <54878400+jeffy34931@users.noreply.github.com>
2019-10-08 23:26:16 +0700

    Add files via upload

---
+++ 7 JumpAround.py
@@ -0,0 +1,11 @@
+"""JumpAround"""
+def main():
+    """calculate x variable"""
+    num = int(input())
+    output_1 = num + 5
+    output_2 = num - 17
+    output_3 = num * 32
+    output_4 = (5 * (num**2)) + (10 * (5 * num)) + 3
+    print("%d\n%d\n%d\n%d\n%d"%(num, output_1, output_2, output_3, output_4))
+
+main()
